70aa23be2a04382e6d728278381606b80dd4a044
rafaelalvesferreira <rafael.alves.ferreira@usp.br>
2020-10-19 14:55:37 -0300

    feat: adding project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final delivery for discipline FEA EAD5754',
+    author='Rafael Alves Ferreira',
+    license='MIT',
+)
